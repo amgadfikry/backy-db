e40bc09bc25b1f868c38ed070812f9d5b260fdf9
amgadfikry <dr.amgad_sh92@yahoo.com>
2025-07-30 17:51:59 +0300

    fix (security_manager): Fix the logic errors and improve architecture
    This commit fix some logic error beside enhancing the architecture of the security manager:
    - Remove inheritance from SecurityEngine as it was not necessary and caused confusion.
    - Add logger to the security manager for better logging.
    - Change the check of integrity as it is part of the configuration now not from the parent class.
    - Change all print statements to logger calls for consistency.
    - Remove unnecessary return type as there is no need to return path from decryption method and
        encryption method.
    - Remove unnecessary nested check of integrity in decryption method as it already handled in
        verify_integrity method.

--- security/security_manager.py
+++ security/security_manager.py
@@ -1,23 +1,27 @@
-# security/security_manger.py
-from security.security_engine import SecurityEngine
+# security/security_manager.py
 from security.encryption_service import EncryptionService
 from security.decryption_service import DecryptionService
 from security.security_metadata import SecurityMetadata
-from pathlib import Path
+from logger.logger_manager import LoggerManager
 
 
-class SecurityManager(SecurityEngine):
+class SecurityManager:
     """
     A composite class that combines security operations, encryption, decryption,
     and metadata handling.
     """
 
     def __init__(self, security_config: dict):
-        super().__init__(security_config)
+        """
+        Initialize the SecurityManager with the provided security configuration.
+        Args:
+            security_config (dict): The security configuration dictionary.
+        """
+        self.logger = LoggerManager.setup_logger("security")
         self.config = security_config
         self.metadata_service = SecurityMetadata(self.config)
 
-    def encryption(self) -> Path:
+    def encryption(self) -> None:
         """
         Perform the encryption process.
         1. Encrypt the file using a symmetric key.
@@ -25,8 +29,6 @@ class SecurityManager(SecurityEngine):
         3. Create metadata for the encrypted files.
         4. Copy the public key to the processing path.
         5. Check the integrity-check if enabled add integrity check file
-        Returns:
-            Path: path of the the folder containing encrypted file, metadata, encryption key, and public key.
         """
         try:
             encryption_service = EncryptionService(self.config)
@@ -34,38 +36,29 @@ class SecurityManager(SecurityEngine):
             encryption_service.encrypt_symmetric_key_with_public_key(symmetric_key)
             self.metadata_service.create_metadata()
             self.metadata_service.copy_public_key()
-            if self.integrity_check:
+            if self.config.get("integrity_check", False):
                 self.metadata_service.create_integrity_file()
-            print(
-                f"Encryption completed, encrypted data and generated metadata, encryption key, and public key in: {self.processing_path}"
-            )
-            return self.processing_path
+            self.logger.info("Encryption completed successfully")
         except Exception as e:
-            print(f"Encryption failed: {e}")
+            self.logger.error(f"Encryption failed: {e}")
             raise e
 
-    def decryption(self) -> Path:
+    def decryption(self) -> None:
         """
         Perform the decryption process.
         1. Decrypt the symmetric key using the private key.
         2. Decrypt the file using the symmetric key.
         3. Return the path of the decrypted file.
-        Returns:
-            Path: path of the decrypted file.
         """
         try:
             decryption_service = DecryptionService(self.config)
-            if self.integrity_check:
-                if not self.metadata_service.verify_integrity():
-                    raise ValueError(
-                        "Integrity check failed. The file may have been tampered with."
-                    )
+            if self.config.get("integrity_check", False):
+                self.metadata_service.verify_integrity()
             symmetric_key = decryption_service.decrypt_symmetric_key()
             decrypted_data_path = decryption_service.decrypt_data(symmetric_key)
-            print(
+            self.logger.info(
                 f"Decryption completed, decrypted file available at: {decrypted_data_path}"
             )
-            return decrypted_data_path
         except Exception as e:
-            print(f"Decryption failed: {e}")
+            self.logger.error(f"Decryption failed: {e}")
             raise e
