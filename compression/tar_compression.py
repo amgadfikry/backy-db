92bb7b9274cd09f0c0e0e45ef858ab4f5c6ae30c
amgadfikry <dr.amgad_sh92@yahoo.com>
2025-08-05 12:51:42 +0300

    feat (tar_compression): Add new methods and enhanced error handling
    This commit introduces new methods for tar compression and decompression bytes, along with improved error handling for various scenarios.
    Also make it deal with single type if tar compression instead of multiple types.
    
    The following changes were made:
    - Added `compress_bytes` method to compress data into a tar file in memory.
    - Added `decompress_bytes` method to decompress data from a tar file in memory.
    - Enhanced error handling for cases where the input is not a file or is corrupted.
    - Enhanced documentation for clarity on the new methods and their usage.

--- compression/tar_compression.py
+++ compression/tar_compression.py
@@ -1,6 +1,7 @@
 # compression/tar_compression.py
 import tarfile
 from pathlib import Path
+import io
 from compression.compression_base import CompressionBase
 from utils.delete_folder import delete_folder
 
@@ -11,61 +12,146 @@ class TarCompression(CompressionBase):
     Inherits from CompressionBase and implements methods for tar-specific operations.
     """
 
-    TAR_READ_MODES = {
-        "tar": "r",
-        "tar.gz": "r:gz",
-    }
-
-    TAR_WRITE_MODES = {
-        "tar": "w",
-        "tar.gz": "w:gz",
-    }
-
-    def compress_folder(self) -> str:
+    def compress_folder(self, folder_path: Path) -> Path:
         """
         Compress the given folder into a .tar archive preserving the folder structure.
         Then remove the original folder.
+        Args:
+            folder_path (Path): Path to the folder to compress.
+        Raises:
+            ValueError: If the folder_path is None or does not exist or is not a directory.
+            RuntimeError: If the tar file creation fails.
         Returns:
-            extension (str): The file extension used for the compressed file.
+            Path: Path to the created tar file.
         """
-        # Assign the folder path name
-        folder_path = self.get_folder_from_processing_path()
-        tar_folder_path = folder_path.with_suffix(f".{self.extension}")
-        tar_type = self.TAR_WRITE_MODES[self.extension]
+        # Check if folder_path is None or does not exist or is not a directory, raise an error
+        if folder_path is None or not folder_path.exists() or not folder_path.is_dir():
+            self.logger.error(
+                "No valid folder path or directory provided for compression."
+            )
+            raise ValueError(
+                "No valid folder path or directory provided for compression."
+            )
 
-        # Create a tar file and add the folder contents
         try:
-            with tarfile.open(tar_folder_path, tar_type) as tarf:
+            # Determine the tar file name based on the folder name and extension
+            compressed_folder_name = folder_path.parent / (
+                folder_path.stem + self.extension
+            )
+            # Compress the folder into a tar file
+            with tarfile.open(compressed_folder_name, "w") as tarf:
                 tarf.add(folder_path, arcname=folder_path.name)
-            self.logger.info(f"Successfully created tar file: {tar_folder_path}")
+            # Log the successful creation of the tar file and delete the original folder
+            self.logger.info(f"Successfully created tar file: {compressed_folder_name}")
+            delete_folder(folder_path)
+            return compressed_folder_name
+
         except Exception as e:
-            self.logger.error(f"Failed to create tar file: {e}")
-            raise RuntimeError("Failed to create tar file") from e
+            self.logger.error(f"Failed to compress tar file: {e}")
+            raise RuntimeError("Failed to compress tar file") from e
 
-        # Remove the original folder after compression
-        delete_folder(folder_path)
-
-        return self.extension
-
-    def decompress_folder(self) -> Path:
+    def decompress_folder(self, folder_path: Path) -> Path:
         """
         Decompress a .tar file to a folder preserving the original structure.
         it will extract to the same directory as the tar file
         Then remove the original tar file.
+        Args:
+            folder_path (Path): Path to the compressed file.
+        Raises:
+            ValueError: If the folder_path is None or does not exist or is not a file.
+            RuntimeError: If the extraction fails.
+        Returns:
             Path: Path to the extracted folder.
         """
-        # Get the tar file from processing path
-        tar_path = self.get_compressed_file_from_processing_path()
-        tar_type = self.TAR_READ_MODES[self.extension]
+        # Check if folder_path is None or does not exist or is not a file, raise an error
+        if folder_path is None or not folder_path.exists() or not folder_path.is_file():
+            self.logger.error("No valid tar file path provided for decompression.")
+            raise ValueError("No valid tar file path provided for decompression.")
 
-        # Extract the tar file to a folder with the same name as the tar file
         try:
-            with tarfile.open(tar_path, tar_type) as tarf:
-                tarf.extractall(path=self.processing_path)
-            self.logger.info(f"Successfully extracted tar file: {tar_path}")
+            extracted_folder = folder_path.parent
+            # Open the tar file and extract its contents
+            with tarfile.open(folder_path, "r") as tarf:
+                tarf.extractall(path=extracted_folder)
+            # Log the successful extraction and delete the original tar file
+            self.logger.info(
+                f"Successfully extracted tar file: {Path(extracted_folder)}"
+            )
+            folder_path.unlink()
+            return extracted_folder / folder_path.stem
+
         except Exception as e:
             self.logger.error(f"Failed to extract tar file: {e}")
             raise RuntimeError("Failed to extract tar file") from e
-        delete_folder(tar_path)
 
-        return self.processing_path / tar_path.stem
+    def compress_bytes(self, data: bytes) -> bytes:
+        """
+        Compress the given bytes using tar compression.
+        Args:
+            data (bytes): Data to compress.
+        Raises:
+            ValueError: If the data is None or empty.
+            RuntimeError: If the compression fails.
+        Returns:
+            bytes: Compressed data.
+        """
+        # Check if data is None or empty, raise an error
+        if not data:
+            self.logger.error("No data provided for compression.")
+            raise ValueError("No data provided for compression.")
+
+        try:
+            # Create a virtual file object in memory to hold the tar data
+            with io.BytesIO() as buf:
+                # Create a tar file in write mode and add the data
+                with tarfile.open(fileobj=buf, mode="w") as tarf:
+                    tar_info = tarfile.TarInfo(name="data")
+                    tar_info.size = len(data)
+                    tarf.addfile(tar_info, fileobj=io.BytesIO(data))
+                # Get the compressed data from the buffer
+                self.logger.info("Successfully compressed bytes into tar format.")
+                return buf.getvalue()
+
+        except Exception as e:
+            self.logger.error(f"Failed to compress bytes with tar: {e}")
+            raise RuntimeError("Failed to compress bytes with tar") from e
+
+    def decompress_bytes(self, data: bytes) -> bytes:
+        """
+        Decompress the given bytes using tar decompression.
+        Args:
+            data (bytes): Data to decompress.
+        Raises:
+            ValueError: If the data is None or empty.
+            RuntimeError: If the decompression fails.
+        Returns:
+            bytes: Decompressed data.
+        """
+        # Check if data is None or empty, raise an error
+        if not data:
+            self.logger.error("No data provided for decompression.")
+            raise ValueError("No data provided for decompression.")
+
+        try:
+            buf = io.BytesIO(data)
+
+            # Open the vitual file object in memory
+            with tarfile.open(fileobj=buf, mode="r") as tarf:
+                members = tarf.getmembers()
+                if not members:
+                    self.logger.error("No files found in tar archive.")
+                    raise ValueError("No files found in tar archive.")
+                fileobj = tarf.extractfile(members[0])
+                if fileobj is None:
+                    self.logger.error("Failed to extract file from tar archive.")
+                    raise ValueError("Failed to extract file from tar archive.")
+                decompressed_data = fileobj.read()
+
+            self.logger.info("Successfully decompressed bytes from tar format.")
+            return decompressed_data
+
+        except Exception as e:
+            if isinstance(e, ValueError):
+                raise e
+            self.logger.error(f"Failed to decompress bytes with tar: {e}")
+            raise RuntimeError("Failed to decompress bytes with tar") from e
