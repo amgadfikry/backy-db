ebf8b984d26a95d1b9648c68f3540747fc1d64a5
amgadfikry <dr.amgad_sh92@yahoo.com>
2025-08-05 08:56:02 +0300

    fix (storage_manager):Add aws storage type and add new methods
    This commit introduces support for AWS S3 storage in the storage manager.
    It adds new operations to manage `delete` and `validate_credentials`.
    
    This change enhances the flexibility of the storage manager, allowing it to handle AWS S3 as a storage option alongside existing
    local storage capabilities.
    
    The following methods have been added:
    - `delete`: Deletes a file from the storage.
    - `validate_credentials`: Validates AWS credentials for S3 storage.

--- storage/storage_manager.py
+++ storage/storage_manager.py
@@ -1,7 +1,7 @@
 # storage/storage_manager.py
 from storage.local_storage import LocalStorage
 from logger.logger_manager import LoggerManager
-from pathlib import Path
+from storage.aws_storage import AWSStorage
 
 
 class StorageManager:
@@ -11,8 +11,10 @@ class StorageManager:
     download, and delete files in the specified storage.
     """
 
+    # Mapping of storage types to their respective classes
     STORAGES = {
         "local": LocalStorage,
+        "aws": AWSStorage,
     }
 
     def __init__(self, config: dict):
@@ -23,23 +25,50 @@ class StorageManager:
         """
         self.logger = LoggerManager.setup_logger("storage")
         storage_type = config.get("storage_type", "local").lower()
+        db_name = config.get("db_name", "backy_db")
+
+        # Validate the storage type is supported
         if storage_type not in self.STORAGES:
             self.logger.error(f"Unsupported storage type: {storage_type}")
             raise ValueError(f"Unsupported storage type: {storage_type}")
-        self.storage = self.STORAGES.get(storage_type)(config)
 
-    def upload(self) -> Path:
-        """
-        Upload a file to the storage.
-        Returns:
-            Path: The path to the uploaded file.
-        """
-        return self.storage.upload()
+        # Initialize the storage instance based on the type
+        self.storage = self.STORAGES.get(storage_type)(db_name)
 
-    def download(self) -> Path:
+    def upload(self, file_path: str) -> str:
         """
-        Download a file from the storage.
+        Upload method that delegates the upload operation to the storage instance.
+        Args:
+            file_path (str): The path to the file to be uploaded.
         Returns:
-            Path: The path to the downloaded file.
+            str: The path to the uploaded file.
         """
-        return self.storage.download()
+        return self.storage.upload(file_path)
+
+    def download(self, file_path: str) -> str:
+        """
+        Download method that delegates the download operation to the storage instance.
+        Args:
+            file_path (str): The path to the file to be downloaded.
+        Returns:
+            str: The path to the downloaded file.
+        """
+        return self.storage.download(file_path)
+
+    def validate_credentials(self) -> bool:
+        """
+        Validate credentials that delegates the validation to the storage instance.
+        Returns:
+            bool: True if credentials are valid, False otherwise.
+        """
+        return self.storage.validate_credentials()
+
+    def delete(self, file_path: str) -> None:
+        """
+        Delete method that delegates the delete operation to the storage instance.
+        Args:
+            file_path (str): The path to the file to be deleted.
+        Raises:
+            RuntimeError: If the delete operation fails.
+        """
+        return self.storage.delete(file_path)
