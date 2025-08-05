3d8f38eec326867302ad1b16f7d175225b2e016c
amgadfikry <dr.amgad_sh92@yahoo.com>
2025-08-05 09:06:12 +0300

    feat (tests fixtures): Add new fixtures to ease the testing process
    This commit introduces new fixtures in the `conftest.py` file to streamline the testing process. The changes include:
    - Function and fixture for checking if there a connection to mysql database and skip tests if not.
    - Fixture to skip the tests need AWS credentials if not set in the environment.
    - Fixture to skip the tests need GCP credentials if not set in the environment.
    - Enhanced the fixture for mocking path to `tmp_path` and setting environment variables for logging and backup directories.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,22 +2,72 @@
 import os
 import pytest
 from pathlib import Path
-import shutil
+import socket
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-@pytest.fixture(scope="session", autouse=True)
-def configure_logging_path():
+def is_mysql_running():
     """
-    Sets up a test-only logging directory and LOGGING_PATH env var.
-    Cleans it up after the test session.
+    Check if MySQL is running by attempting to connect to the default port.
+    Returns:
+        bool: True if MySQL is running, False otherwise.
     """
-    test_log_path = Path.cwd() / "logs" / "backydb"
-    os.environ["LOGGING_PATH"] = str(test_log_path)
-    test_log_path.mkdir(parents=True, exist_ok=True)
+    try:
+        with socket.create_connection(("localhost", 3306), timeout=3):
+            return True
+    except (socket.timeout, ConnectionRefusedError, OSError):
+        return False
 
+@pytest.fixture
+def require_mysql():
+    """
+    Fixture to skip tests if MySQL is not running.
+    This fixture checks if MySQL is running and skips the test if it is not.
+    """
+    if not is_mysql_running():
+        pytest.skip("⚠️ Skipping test: MySQL is not running.")
+
+@pytest.fixture
+def require_aws_credentials():
+    """
+    Fixture to skip tests if AWS credentials are not set.
+    This fixture checks for the presence of AWS environment variables
+    and skips the test if they are not set.
+    """
+    aws_key = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_secret = os.getenv("AWS_SECRET_ACCESS_KEY")
+    if not aws_key or not aws_secret:
+        pytest.skip("⚠️ Skipping test: Missing AWS credentials.")
+
+@pytest.fixture
+def require_gcp_credentials():
+    """
+    Fixture to skip tests if GCP credentials are not set.
+    This fixture checks for the presence of the GCP credentials file environment variable
+    and skips the test if it is not set or the file does not exist.
+    """
+    gcp_cred_var = "GOOGLE_APPLICATION_CREDENTIALS"
+    if not os.getenv(gcp_cred_var) or not Path(os.getenv(gcp_cred_var)).exists():
+        pytest.skip("⚠️ Skipping test: Missing or invalid GCP credentials file.")
+
+@pytest.fixture(autouse=True)
+def configure_logging_path(tmp_path, monkeypatch):
+    """
+    Fixture to set up the logging path and main backup path for tests.
+    This fixture creates temporary directories for logging and backups,
+    and sets the corresponding environment variables.
+    """
+    # change home directory to tmp_path
+    monkeypatch.setenv("HOME", str(tmp_path))
+    monkeypatch.setattr(Path, "home", lambda: tmp_path)
+    # Create logging and backup directories and set environment variables
+    logging_path = tmp_path / "logs"
+    logging_path.mkdir(parents=True, exist_ok=True)
+    monkeypatch.setenv("LOGGING_PATH", str(logging_path))
+    # Create the main backup directory and set the environment variable
+    process_path = tmp_path / "backups"
+    process_path.mkdir(parents=True, exist_ok=True)
+    monkeypatch.setenv("MAIN_BACKUP_PATH", str(process_path))
     yield
-
-    # Teardown: only delete if we know it's the test path
-    if test_log_path.parent.exists() and test_log_path.parent.is_dir():
-        shutil.rmtree(test_log_path.parent, ignore_errors=True)
-    os.environ.pop("LOGGING_PATH", None)
